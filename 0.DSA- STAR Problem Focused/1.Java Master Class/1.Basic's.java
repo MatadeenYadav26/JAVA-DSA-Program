d3c896b2bb37c7f779ac33a1bb34eec98ffc5e67
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-07-10 17:38:46 +0530

    Create Switch class with switch statement
    Add a Switch class with a basic switch statement.

---
+++ 0.DSA- STAR Problem Focused/1.Java Master Class/1.Basic's.java
@@ -0,0 +1,23 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Switch {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+        switch (value) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("default");
+        }
+    }
+}
